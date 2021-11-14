c165318a7dade9c72303fdb0585b670d40786e82
Vineet Sridhar <vineets1600@gmail.com>
2021-11-13 21:06:50 -0500

    Added web gui

--- gui.py
+++ gui.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+from threading import Lock
 
-@app.route("/")
-def send_hi():
-    return render_template("index.html")
+
+async_mode = None
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socket_ = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socket_.async_mode)
+
+
+@socket_.on('connect', namespace='/notes')
+def connect():
+    session['is_listening'] = False
+    return emit('my_response',
+         {'count':3, "data":"poo" })
+
+@socket_.on('button_press', namespace='/notes')
+def button_press():
+    session['is_listening'] = not session.get('is_listening')
+    if session.get('is_listening'):
+        print("Start listening")
+    else:
+        print("Stop listening")
+    return emit('my_response',
+         {'is_listening':session.get('is_listening') })
+
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    socket_.run(app, debug=True)
